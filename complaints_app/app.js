ac63210072fd7be7988779cc58250642a85dbd57
Eliza Li <lie2@bxscience.edu>
2022-05-26 13:27:27 -0400

    template

---
+++ complaints_app/app.js
@@ -0,0 +1,7 @@
+// fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json')
+// .then(response => response.json()) //parting res to json
+// .then(users => {
+//     console.log(users);
+// })
+// // .then(json => console.log(json)) //json data
+// .catch(err => console.log(err)) //handle errors
